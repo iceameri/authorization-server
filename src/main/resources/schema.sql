e290c55bf7e793a3a9d09a810724356b83ed63a3
icedcoldbrew <jwoo5264@gmail.com>
2025-12-30 17:59:51 +0900

    build: opaque-client 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -100,7 +100,32 @@ VALUES ('1',
         'http://localhost:9090/auth/callback,http://localhost:9091/authorized,http://localhost:9090/auth/authorized',
         'openid,profile,email',
         '{"@class":"java.util.Collections$UnmodifiableMap","settings.client.require-proof-key":false,"settings.client.require-authorization-consent":false}',
-        '{"@class": "java.util.Collections$UnmodifiableMap","settings.token.authorization-code-time-to-live": ["java.time.Duration", 300.000000000],"settings.token.access-token-time-to-live": ["java.time.Duration", 1800.000000000],"settings.token.refresh-token-time-to-live": ["java.time.Duration", 86400.000000000],"settings.token.reuse-refresh-tokens": ["java.lang.Boolean", false],"settings.token.device-code-time-to-live": ["java.time.Duration", 300.000000000]}');
+        '{"@class": "java.util.Collections$UnmodifiableMap","settings.token.authorization-code-time-to-live": ["java.time.Duration", 300.000000000],"settings.token.access-token-time-to-live": ["java.time.Duration", 1800.000000000],"settings.token.refresh-token-time-to-live": ["java.time.Duration", 86400.000000000],"settings.token.reuse-refresh-tokens": ["java.lang.Boolean", false],"settings.token.device-code-time-to-live": ["java.time.Duration", 300.000000000]}'),
+      (
+        '2',
+        'opaque-client',
+        GETDATE(),
+        '{bcrypt}$2a$10$vNyUhWG2.Gd70U4zW9runOVnKaNmY9/DxcAdLweXqHch48eEYdO7i', -- 비번: P@$$w0rd1!
+        'opaque-client-service',
+        'client_secret_basic,client_secret_post',
+        'authorization_code,refresh_token,client_credentials',
+        'http://localhost:9092/callback',
+        'openid,profile,email',
+        '{"@class":"java.util.Collections$UnmodifiableMap",
+          "settings.client.require-proof-key":false,
+          "settings.client.require-authorization-consent":false
+         }',
+        -- ▼ token_settings: access token 형식 = opaque
+        '{
+            "@class": "java.util.Collections$UnmodifiableMap",
+            "settings.token.authorization-code-time-to-live": ["java.time.Duration", 300.000000000],
+            "settings.token.access-token-time-to-live": ["java.time.Duration", 1800.000000000],
+            "settings.token.refresh-token-time-to-live": ["java.time.Duration", 86400.000000000],
+            "settings.token.reuse-refresh-tokens": ["java.lang.Boolean", false],
+            "settings.token.device-code-time-to-live": ["java.time.Duration", 300.000000000],
+            "settings.token.access-token-format": ["org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat", "opaque"]
+         }'
+        );
 
 
 USE resource;
