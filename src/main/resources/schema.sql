602ce2f77a95adc64c9b915624062cc48a201a40
icedcoldbrew <jwoo5264@gmail.com>
2026-01-09 09:53:13 +0900

    chore: 주석추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -30,17 +30,17 @@ CREATE TABLE oauth2_authorization
     oidc_id_token_expires_at      DATETIME       NULL,
     oidc_id_token_metadata        NVARCHAR(MAX)  NULL,
 
-    refresh_token_value           NVARCHAR(MAX)  NULL,
+    refresh_token_value           NVARCHAR(MAX)  NULL,     -- Refresh Token
     refresh_token_issued_at       DATETIME       NULL,
     refresh_token_expires_at      DATETIME       NULL,
     refresh_token_metadata        NVARCHAR(MAX)  NULL,
 
-    user_code_value               NVARCHAR(MAX)  NULL,
+    user_code_value               NVARCHAR(MAX)  NULL, -- 사용자가 직접 입력하는 코드
     user_code_issued_at           DATETIME       NULL,
     user_code_expires_at          DATETIME       NULL,
     user_code_metadata            NVARCHAR(MAX)  NULL,
 
-    device_code_value             NVARCHAR(MAX)  NULL,
+    device_code_value             NVARCHAR(MAX)  NULL, -- 서버가 발급한 Device Code
     device_code_issued_at         DATETIME       NULL,
     device_code_expires_at        DATETIME       NULL,
     device_code_metadata          NVARCHAR(MAX)  NULL,
@@ -65,18 +65,18 @@ CREATE TABLE oauth2_authorization_consent
 CREATE TABLE oauth2_registered_client
 (
     id                            VARCHAR(100) NOT NULL,
-    client_id                     VARCHAR(100) NOT NULL,
-    client_id_issued_at           DATETIME     NOT NULL,
-    client_secret                 VARCHAR(200),
-    client_secret_expires_at      DATETIME,
-    client_name                   VARCHAR(200) NOT NULL,
-    client_authentication_methods VARCHAR(MAX) NOT NULL,
-    authorization_grant_types     VARCHAR(MAX) NOT NULL,
-    redirect_uris                 VARCHAR(MAX),
-    post_logout_redirect_uris     VARCHAR(MAX),
-    scopes                        VARCHAR(MAX) NOT NULL,
-    client_settings               VARCHAR(MAX) NOT NULL,
-    token_settings                VARCHAR(MAX) NOT NULL,
+    client_id                     VARCHAR(100) NOT NULL, -- 외부에 공개되는 클라이언트 식별자
+    client_id_issued_at           DATETIME     NOT NULL, -- Client ID가 발급된 시점
+    client_secret                 VARCHAR(200),          -- Confidential Client에서 사용
+    client_secret_expires_at      DATETIME,              -- Client Secret 만료 시각
+    client_name                   VARCHAR(200) NOT NULL, -- 클라이언트의 표시 이름
+    client_authentication_methods VARCHAR(MAX) NOT NULL, -- 클라이언트 인증 방식 ex client_secret_basic, client_secret_post, none
+    authorization_grant_types     VARCHAR(MAX) NOT NULL, -- 클라이언트가 사용할 수 있는 인증 흐름 ex authorization_code, client_credentials, refresh_token
+    redirect_uris                 VARCHAR(MAX),          -- 로그인 후 인가 서버가 리다이렉트할 주소
+    post_logout_redirect_uris     VARCHAR(MAX),          -- 로그아웃 후 리다이렉트 URI, OpenID Connect 로그아웃 이후 이동할 주소
+    scopes                        VARCHAR(MAX) NOT NULL, -- 클라이언트가 요청 가능한 권한 범위  ex openid, profile read, write
+    client_settings               VARCHAR(MAX) NOT NULL, -- 클라이언트 관련 추가 설정 (JSON), ex PKCE 사용 여부, 사용자 동의 화면 필요 여부
+    token_settings                VARCHAR(MAX) NOT NULL, -- 토큰 관련 설정 (JSON)
     CONSTRAINT [PK_oauth2_registered_client] PRIMARY KEY CLUSTERED (id ASC),
     CONSTRAINT [UK_oauth2_registered_client_01] UNIQUE NONCLUSTERED (client_id ASC)
 );
@@ -98,13 +98,13 @@ VALUES ('1',
         '{bcrypt}$2a$10$vNyUhWG2.Gd70U4zW9runOVnKaNmY9/DxcAdLweXqHch48eEYdO7i', -- P@$$w0rd1!
         'test',
         'client_secret_basic,client_secret_post,client_secret_jwt,private_key_jwt',
-        'authorization_code,refresh_token,client_credentials,urn:ietf:params:oauth:grant-type:device_code',
-        'http://localhost:9090/oauth2/authorize,http://localhost:9090/oauth2/token,http://localhost:9090/auth/authorized',
+        'authorization_code,refresh_token,client_credentials,urn:ietf:params:oauth:grant-type:device_code', -- 입력가능한 타입 예시 추가(해당 로직에 대한 구현 X)
+        'http://localhost:9090/oauth2/authorize,http://localhost:9090/oauth2/token,http://localhost:9090/auth/authorized', -- /oauth2/** 기본 프레임워크에서 지원, 커스텀은 로직을 보기 위해 구현
         'openid,profile,email',
         '{"@class":"java.util.Collections$UnmodifiableMap",
-            "settings.client.require-proof-key":false,
+            "settings.client.require-proof-key":false, --
             "settings.client.require-authorization-consent":false
-            }',
+            }',-- proof-key: PKCE 사용 여부 (Public Client에서 필수) authorization-consent: 사용자에게 동의(consent) 화면을 보여줄지 여부
         '{
             "@class": "java.util.Collections$UnmodifiableMap",
             "settings.token.authorization-code-time-to-live": ["java.time.Duration", 300.000000000],
@@ -136,7 +136,8 @@ VALUES ('1',
             "settings.token.device-code-time-to-live": ["java.time.Duration", 300.000000000],
             "settings.token.access-token-format": ["org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat", "reference"]
          }');
-
+-- OAuth2TokenFormat opaque token 사욜할때만 선언 기본값은 self-contained
+-- client_settings 모두 false 인 이유는 현재 프로젝트에서 jwt와 opaque token 두 개의 서버를 구현하기 위해 다른 개발은 하지 않음.
 
 CREATE TABLE user_details
 (
@@ -188,9 +189,9 @@ VALUES ('admin',
 CREATE TABLE roles
 (
     role_id     BIGINT IDENTITY (1,1) NOT NULL,
-    role_name   VARCHAR(50)           NOT NULL, -- USER, ADMIN, OPERATOR 등
-    description NVARCHAR(255)         NULL,
-    created_at  DATETIME              NOT NULL DEFAULT GETDATE(),
+    role_name   VARCHAR(50)           NOT NULL,                   -- USER, ADMIN, OPERATOR 등
+    description NVARCHAR(255)         NULL,                       -- 설명
+    created_at  DATETIME              NOT NULL DEFAULT GETDATE(), -- 생성일시
     CONSTRAINT PK_roles PRIMARY KEY CLUSTERED (role_id),
     CONSTRAINT UK_roles_name UNIQUE (role_name)
 );
